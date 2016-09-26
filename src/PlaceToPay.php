585faefa0e9a03f4bf1852a63c242c8cf192a401
Sebastian Fernandez <sebas@spacemonkeys.agency>
2016-09-26 14:21:44 -0500

    Getting transition info

--- src/PlaceToPay.php
+++ src/PlaceToPay.php
@@ -22,7 +22,7 @@ class PlaceToPay {
 		return $response->getBankListResult->item;
 	}
 
-	public static function createTransaction( PSETransactionRequest $request ) {
+	public static function createTransaction( PSETransactionRequest $request, $saveOnSession = false, $sessionVar = 'transactionIds' ) {
 		$client   = new SoapClient( self::$wsdl );
 		$response = $client->__soapCall( 'createTransaction', array(
 			[
@@ -30,7 +30,26 @@ class PlaceToPay {
 				'transaction' => $request,
 			]
 		) );
+		if ( $saveOnSession ) {
+			session_start();
+			$transactionIds          = empty( $_SESSION[ $sessionVar ] ) ? [] : $_SESSION[ $sessionVar ];
+			$transactionIds[] = $response->createTransactionResult->transactionID;
+			$_SESSION[ $sessionVar ] = $transactionIds;
+		}
+
 		return $response->createTransactionResult;
+	}
+
+	public static function getTransactionInformation( $transactionID ) {
+		$client   = new SoapClient( self::$wsdl );
+		$response = $client->__soapCall( 'getTransactionInformation', array(
+			[
+				'auth'          => self::$auth,
+				'transactionID' => $transactionID,
+			]
+		) );
+
+		return $response->getTransactionInformationResult;
 	}
 
 	public static function printRequestForm( $url ) {
